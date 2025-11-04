fc90321d2f3a6cfd46d50cf19f019478fb1418f0
Bruno <dias.b@academico.ifrn.edu.br>
2025-11-04 13:22:24 +0000

    iniciando transacoes

--- mysite/baseteste/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from baseteste.views import base_index
-
-urlpatterns = [
-    path('', base_index, name='indexteste'),
-]
--- mysite/baseteste/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def base_index(request):
-    return render(request, 'indexteste.html')
