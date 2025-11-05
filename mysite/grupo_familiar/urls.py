a7bf07ce11bba3d799472b70ac648979ed9dc2e0
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2025-11-05 10:14:34 -0300

    ajustes nas linkagens e em alguns app names de templates

--- mysite/grupo_familiar/urls.py
+++ mysite/grupo_familiar/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from grupo_familiar import views
+
+app_name = 'familia'
+
 urlpatterns = [
-    path('',views.index,name='home_familia')
+    path('', views.familia, name='familia')
 ]
