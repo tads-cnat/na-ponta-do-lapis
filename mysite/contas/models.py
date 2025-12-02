80fc064280157e07332ff2f10142cd9b34d1ba85
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2025-12-02 20:37:28 -0300

    Pequenos ajustes antes do merge

--- mysite/contas/models.py
+++ mysite/contas/models.py
@@ -6,9 +6,9 @@ from usuario.models import Usuario
 
 class ContaFinanceira(Model):
     TIPOS_CONTA = (('CREDITO', 'Crédito'), ('CREDITO/DEBITO', 'Crédito/Débito'), ('DEBITO', 'Débito'))
-    nome = CharField(max_length=100, blank=False, null=False, verbose_name='Nome da Conta')
-    saldo = FloatField(default=0, blank=False, null=False, verbose_name='Saldo da Conta')
-    tipo = CharField(max_length=14, choices=TIPOS_CONTA, default='', blank=False, null=False, verbose_name='Tipo da Conta')
+    nome = CharField('Nome da Conta', max_length=100, blank=False, null=False)
+    saldo = FloatField('Saldo da Conta', default=0.0, blank=False, null=False)
+    tipo = CharField('Tipo da Conta', max_length=14, choices=TIPOS_CONTA, default='', blank=False, null=False)
     id_usuario = ForeignKey(Usuario, on_delete=CASCADE, blank=False, null=False, related_name="contas_financeiras", verbose_name='Usuário da Conta')
 
     class Meta:
@@ -38,21 +38,6 @@ class ContaFinanceira(Model):
         
         if erros:
             raise ValidationError(erros)
-
-    def set_nome(self, novo_nome):
-        if (not isinstance(novo_nome, str) or novo_nome == ""):
-            raise ValueError("O nome deve ser uma string e não vazio")
-        self.nome = novo_nome
-
-    def set_tipo(self, novo_tipo):
-        if (not isinstance(novo_tipo, str) or novo_tipo == "" and novo_tipo != 'CREDITO' and novo_tipo != 'CREDITO/DEBITO' and novo_tipo != 'DEBITO'):
-            raise ValueError("O tipo da conta deve ser um string não vazia e de um tipo válido")
-        self.tipo = novo_tipo
-
-    def set_saldo(self, novo_saldo):
-        if (not isinstance(novo_saldo, float)):
-            raise ValueError("O saldo deve ser um valor real")
-        self.saldo = novo_saldo
 
     def __str__(self):
         return f"{self.nome} - {self.saldo} - {self.tipo}"
