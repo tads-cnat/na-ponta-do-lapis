9e04649be5bff0bc9c9db62ea901d2fc4f5af5df
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-11 19:56:01 -0300

    fix: corrigindo pacote do controller de conta

--- npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/Controller/ContaFinanceiraController.java
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/Controller/ContaFinanceiraController.java
@@ -1,12 +1,18 @@
-package com.npl.na_ponta_do_lapis.controller;
+package com.npl.na_ponta_do_lapis.web.Controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.npl.na_ponta_do_lapis.entity.ContaFinanceira;
 import com.npl.na_ponta_do_lapis.service.ContaFinanceiraService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/contas")
