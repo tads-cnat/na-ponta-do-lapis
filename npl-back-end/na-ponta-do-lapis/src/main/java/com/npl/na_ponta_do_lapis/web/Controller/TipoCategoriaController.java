79551ffc9c1d0e6448122a7ee21b007380b67ab4
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2026-04-12 18:30:11 -0300

    feat:implementação de categoria completa

---
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/Controller/TipoCategoriaController.java
@@ -0,0 +1,50 @@
+package com.npl.na_ponta_do_lapis.web.Controller;
+
+import com.npl.na_ponta_do_lapis.entity.TipoCategoria;
+import com.npl.na_ponta_do_lapis.service.TipoCategoraService;
+import com.npl.na_ponta_do_lapis.web.dto.TipoCategoriaDTO;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+@Tag(name = "Categorias", description = "Gerenciamento de CRUD categorias")
+public class TipoCategoriaController {
+
+    private TipoCategoraService tipoCategoriaService;
+
+    public TipoCategoriaController(TipoCategoraService tipoCategoraService){
+        this.tipoCategoriaService = tipoCategoraService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<TipoCategoria>> listarCategorias(){
+        return ResponseEntity.status(HttpStatus.OK).body(tipoCategoriaService.listarCategorias());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TipoCategoria> buscarPorId(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(tipoCategoriaService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TipoCategoriaDTO> criarCategoria(@RequestBody @Valid TipoCategoriaDTO tipoCategoriaDTO){
+        TipoCategoria tipoCategoria = tipoCategoriaDTO.toEntity();
+        tipoCategoriaService.criarCategoria(tipoCategoria);
+     return ResponseEntity.status(HttpStatus.CREATED).body(tipoCategoriaDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCategoria(@PathVariable Long id){
+        tipoCategoriaService.deletarCategoria(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
+
+
--- npl-back-end/na-ponta-do-lapis/src/main/resources/data.sql
+++ npl-back-end/na-ponta-do-lapis/src/main/resources/data.sql
@@ -1,9 +1,16 @@
 -- SQL Usuario
-INSERT INTO usuario (nome, email, username, senha, foto_perfil, papel) VALUES
-                    ('Lucas Henrique','lucas@gmail.com', 'Lucaasshq', '123456', 'http:foto.com', 'ADMIN_SITE');
-INSERT INTO usuario (nome, username, email, senha, foto_perfil, papel) VALUES
-    ('Bruno', 'Brunozz','bruno@gmail.com', '123456', 'http:foto.com', 'ADMIN_FAMILIA');
-INSERT INTO usuario (nome, username,email, senha, foto_perfil, papel) VALUES
-    ('Eduardo', 'Dudurant', 'dudu@gmail.com', '123456', 'http:foto.com', 'ADMIN_SITE');
-INSERT INTO usuario (nome, username,email, senha, foto_perfil, papel) VALUES
-    ('Pedro', 'Predo','pedro@gmail.com', '123456', 'http:foto.com', 'USUARIO');
+INSERT INTO usuario (nome, email, username, senha, foto_perfil, papel)
+VALUES ('Lucas Henrique', 'lucas@gmail.com', 'Lucaasshq', '123456', 'http:foto.com', 'ADMIN_SITE');
+INSERT INTO usuario (nome, username, email, senha, foto_perfil, papel)
+VALUES ('Bruno', 'Brunozz', 'bruno@gmail.com', '123456', 'http:foto.com', 'ADMIN_FAMILIA');
+INSERT INTO usuario (nome, username, email, senha, foto_perfil, papel)
+VALUES ('Eduardo', 'Dudurant', 'dudu@gmail.com', '123456', 'http:foto.com', 'ADMIN_SITE');
+INSERT INTO usuario (nome, username, email, senha, foto_perfil, papel)
+VALUES ('Pedro', 'Predo', 'pedro@gmail.com', '123456', 'http:foto.com', 'USUARIO');
+-- SQL Categoria
+INSERT INTO tipo_categoria (nome)
+VALUES ('Alimentação'),
+       ('Transporte'),
+       ('Lazer'),
+       ('Saúde'),
+       ('Educação');
