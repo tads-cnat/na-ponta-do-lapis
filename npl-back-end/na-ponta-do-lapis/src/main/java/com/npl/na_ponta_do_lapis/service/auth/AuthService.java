69e5dad107eb986a68420ac2186bf8132205fc80
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2026-04-12 16:16:27 -0300

    feat(auth ): criando interface Auth e AuthImpl

---
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/service/auth/AuthService.java
@@ -0,0 +1,12 @@
+package com.npl.na_ponta_do_lapis.service.auth;
+
+import com.npl.na_ponta_do_lapis.web.Controller.dto.UsuarioDTO;
+
+public interface AuthService {
+
+    void criarContaAdmin();
+
+    UsuarioDTO cadastrarUsuario();
+
+    boolean hasUserWithEmail(String email);
+}
---
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/service/auth/AuthServiceImpl.java
@@ -0,0 +1,20 @@
+package com.npl.na_ponta_do_lapis.service.auth;
+
+import com.npl.na_ponta_do_lapis.web.Controller.dto.UsuarioDTO;
+
+public class AuthServiceImpl implements AuthService{
+    @Override
+    public void criarContaAdmin() {
+
+    }
+
+    @Override
+    public UsuarioDTO cadastrarUsuario() {
+        return null;
+    }
+
+    @Override
+    public boolean hasUserWithEmail(String email) {
+        return false;
+    }
+}
