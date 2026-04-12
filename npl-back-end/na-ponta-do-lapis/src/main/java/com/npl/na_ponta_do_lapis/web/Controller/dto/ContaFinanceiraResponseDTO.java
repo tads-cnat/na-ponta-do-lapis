be437fdc80ea106182ae6137cc49b217b74b548f
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-12 10:49:18 -0300

    feat: criando o dto de conta

---
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/Controller/dto/ContaFinanceiraResponseDTO.java
@@ -0,0 +1,14 @@
+package com.npl.na_ponta_do_lapis.web.Controller.dto;
+
+import java.math.BigDecimal;
+
+import com.npl.na_ponta_do_lapis.entity.ContaFinanceira;
+import com.npl.na_ponta_do_lapis.entity.Enums.TipoConta;
+import com.npl.na_ponta_do_lapis.entity.Usuario;
+
+public record ContaFinanceiraResponseDTO(Long id, String nome, BigDecimal saldo, TipoConta tipo, Usuario usuario) {
+
+    public ContaFinanceiraResponseDTO(ContaFinanceira contaFinanceira){
+        this(contaFinanceira.getId(), contaFinanceira.getNome(), contaFinanceira.getSaldo(), contaFinanceira.getTipo(), contaFinanceira.getUsuario());
+    }
+}
