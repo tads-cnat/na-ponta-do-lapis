31eee8fb4d3bc408ffb7fb5436ec3d1532a559dd
Pedro <rpedroricardo87@gmail.com>
2026-04-12 15:25:47 -0300

    feat: add familia repository

---
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/FamiliaRepository.java
@@ -0,0 +1,7 @@
+package com.npl.na_ponta_do_lapis.repository;
+
+import com.npl.na_ponta_do_lapis.entity.Familia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FamiliaRepository extends JpaRepository<Familia, Long> {
+}
