28d300af3352c34170517c443dd638d83a5e01d4
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-14 20:32:46 -0300

    feat: adicionando restrições dos atributos da entidade no dto

--- npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/dto/ContaFinanceiraDTO.java
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/dto/ContaFinanceiraDTO.java
@@ -6,13 +6,30 @@ import com.npl.na_ponta_do_lapis.entity.ContaFinanceira;
 import com.npl.na_ponta_do_lapis.entity.Enums.TipoConta;
 import com.npl.na_ponta_do_lapis.entity.Usuario;
 
-public record ContaFinanceiraDTO(String nome, BigDecimal saldo, TipoConta tipo, Usuario usuario) {
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ContaFinanceiraDTO(
+    
+    @NotBlank(message = "O nome da conta não pode ser em branco")
+    String nome,
+    
+    @NotNull(message = "O saldo é obrigatório")
+    @DecimalMin(value = "0.0", inclusive = true, message = "O saldo não pode ser negativo")
+    BigDecimal saldo,
+    
+    @NotNull(message = "O tipo de conta é obrigatório")
+    TipoConta tipo,
+    
+    @NotNull(message = "O usuário é obrigatório")
+    Usuario usuario) {
 
     public ContaFinanceira toEntity(){
         ContaFinanceira conta = new ContaFinanceira();
-        conta.setNome(nome);
-        conta.setSaldo(saldo);
-        conta.setTipo(tipo);
+        conta.setNome(this.nome);
+        conta.setSaldo(this.saldo);
+        conta.setTipo(this.tipo);
         conta.setUsuario(usuario);
         return conta;
     }
