424b81040590fc07ac4fff2de5a2045c645df0d7
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-14 21:27:50 -0300

    feat: adicionando linhas na tabela de conta financeira usando puro sql

--- npl-back-end/na-ponta-do-lapis/src/main/resources/data.sql
+++ npl-back-end/na-ponta-do-lapis/src/main/resources/data.sql
@@ -14,3 +14,25 @@ VALUES ('Alimentação'),
        ('Lazer'),
        ('Saúde'),
        ('Educação');
+
+-- SQL Conta Financeira
+INSERT INTO conta_financeira (nome, saldo, tipo, usuario)
+VALUES ('Banco Inter', 100, 'DEBITO', 1);
+
+INSERT INTO conta_financeira (nome, saldo, tipo, usuario)
+VALUES ('Banco Master', 50000000.50, 'CREDITO', 2);
+
+INSERT INTO conta_financeira (nome, saldo, tipo, usuario)
+VALUES ('Santander', 0, 'CREDITO_DEBITO', 3);
+
+INSERT INTO conta_financeira (nome, saldo, tipo, usuario)
+VALUES ('Itaú', 150, 'DEBITO', 4);
+
+INSERT INTO conta_financeira (nome, saldo, tipo, usuario)
+VALUES ('Nubank', 3000, 'DEBITO', 3);
+
+INSERT INTO conta_financeira (nome, saldo, tipo, usuario)
+VALUES ('BB', 10, 'CREDITO', 2);
+
+INSERT INTO conta_financeira (nome, saldo, tipo, usuario)
+VALUES ('PicPay', 1, 'CREDITO', 1);
