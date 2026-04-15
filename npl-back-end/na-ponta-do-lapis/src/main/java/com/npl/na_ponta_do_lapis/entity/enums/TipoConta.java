d8cd89c8ffc36f015ffa4f0a2b6786dabdb08e80
Lucas H B Medeiros <120293165+Lucaasshq@users.noreply.github.com>
2026-04-14 21:06:59 -0300

    corrigindo imports e pacotes

--- npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/entity/enums/TipoConta.java
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/entity/enums/TipoConta.java
@@ -2,11 +2,19 @@ package com.npl.na_ponta_do_lapis.entity.enums;
 
 public enum TipoConta {
 
+<<<<<<< HEAD
     CREDITO("Crédito"),
     DEBITO("Débito"),
     CREDITO_DEBITO("Crédito/Débito");
 
     private final String tipo;
+=======
+    CREDITO("Credito"),
+    DEBITO("Debito"),
+    CREDITO_DEBITO("Credito Debito");
+
+    private String tipo;
+>>>>>>> 0e87127 (corrigindo imports e pacotes)
 
     private TipoConta(String tipo) {
         this.tipo = tipo;
--- npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/controller/MetaController.java
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/controller/MetaController.java
@@ -1,5 +1,6 @@
 package com.npl.na_ponta_do_lapis.web.controller;
 
+<<<<<<< HEAD
 
 import com.npl.na_ponta_do_lapis.service.MetaService;
 import com.npl.na_ponta_do_lapis.web.dto.MetaDTO;
@@ -9,6 +10,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+=======
+import com.npl.na_ponta_do_lapis.entity.Meta;
+import com.npl.na_ponta_do_lapis.service.MetaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+>>>>>>> 0e87127 (corrigindo imports e pacotes)
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +31,7 @@ public class MetaController {
     }
 
     @Operation(summary = "Listar metas")
+<<<<<<< HEAD
     @GetMapping
     public ResponseEntity<List<MetaResponseDTO>> listar(){
         List<MetaResponseDTO> response = service.listarMetas();
@@ -35,5 +43,16 @@ public class MetaController {
     public ResponseEntity<MetaResponseDTO> criar(@RequestBody @Valid MetaDTO metaDTO){
         MetaResponseDTO novaMeta = service.criarMeta(metaDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaMeta);
+=======
+    @PostMapping
+    public Meta criar(@RequestBody Meta meta){
+        return service.criarMeta(meta);
+    }
+
+    @Operation(summary = "Criar metas")
+    @GetMapping
+    public List<Meta> listar(){
+        return service.listarMetas();
+>>>>>>> 0e87127 (corrigindo imports e pacotes)
     }
 }
