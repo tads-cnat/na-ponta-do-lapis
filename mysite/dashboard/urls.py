ccf3bb48cb6d19f80c0ee1b4d81c7d78aa9e5274
Bruno <dias.b@academico.ifrn.edu.br>
2026-04-16 22:43:33 +0000

    chore: aplica regras do gitigore

--- mysite/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from . import views
-
-app_name = "dashboard"
-
-urlpatterns = [
-    path('',views.dashboard_view, name="dashboard"),
-]
--- mysite/usuario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('',views.login_usuario, name="login"),
-    path('cadastro/',views.cadastro_usuario, name="cadastro"),
-    path('logout/',views.logout_usuario, name="logout"),
-]
