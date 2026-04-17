0737b6bb61fbac20bc62225ca49514e6409e8347
Bruno <dias.b@academico.ifrn.edu.br>
2026-04-17 01:28:27 +0000

    chore: altera o nome do banco e desativa testes

--- backend/na-ponta-do-lapis/src/test/java/com/npl/na_ponta_do_lapis/NaPontaDoLapisApplicationTests.java
+++ backend/na-ponta-do-lapis/src/test/java/com/npl/na_ponta_do_lapis/NaPontaDoLapisApplicationTests.java
@@ -1,9 +1,11 @@
 package com.npl.na_ponta_do_lapis;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Testes Desativados")
 class NaPontaDoLapisApplicationTests {
 
 	@Test
