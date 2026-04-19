11e6f97ceda776af026638b1e430c67f299c9e08
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-19 18:10:00 -0300

    feat: alterando o tipo de erro que a exceção de buscar id e não contrar

--- npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/exception/ContaIdNaoExisteException.java
+++ npl-back-end/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/exception/ContaIdNaoExisteException.java
@@ -1,5 +1,9 @@
 package com.npl.na_ponta_do_lapis.web.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ContaIdNaoExisteException extends RuntimeException {
     public ContaIdNaoExisteException(String message) {
         super(message);
