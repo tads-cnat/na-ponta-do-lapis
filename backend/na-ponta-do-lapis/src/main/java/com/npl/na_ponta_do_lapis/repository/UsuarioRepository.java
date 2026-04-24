5405612356e5c817fb6b8e41cd9de2b896547b76
Pedro <rpedroricardo87@gmail.com>
2026-04-24 16:55:18 -0300

    feat: add findByUsername

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/UsuarioRepository.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/UsuarioRepository.java
@@ -4,5 +4,8 @@ package com.npl.na_ponta_do_lapis.repository;
 import com.npl.na_ponta_do_lapis.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
 }
