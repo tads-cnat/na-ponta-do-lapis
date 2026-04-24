513fbb3f9b442a680bc85425cc39f905c55de665
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-04-24 16:04:58 -0300

    fix: corrgindo novamente criação do banco de dados via script

--- backend/na-ponta-do-lapis/src/main/resources/data.sql
+++ backend/na-ponta-do-lapis/src/main/resources/data.sql
@@ -4,8 +4,7 @@ VALUES
     (1, 'Lucas Henrique', 'lucas@gmail.com', 'Lucaasshq', '123456', 'http://foto.com', 'ADMIN_SITE'),
     (2, 'Bruno', 'bruno@gmail.com', 'Brunozz', '123456', 'http://foto.com', 'ADMIN_FAMILIA'),
     (3, 'Eduardo', 'dudu@gmail.com', 'Dudurant', '123456', 'http://foto.com', 'ADMIN_SITE'),
-    (4, 'Pedro', 'pedro@gmail.com', 'Predo', '123456', 'http://foto.com', 'USUARIO')
-ON CONFLICT (id) DO NOTHING;
+    (4, 'Pedro', 'pedro@gmail.com', 'Predo', '123456', 'http://foto.com', 'USUARIO');
 
 -- SQL Categoria
 INSERT INTO tipo_categoria (id, nome)
@@ -14,8 +13,7 @@ VALUES
     (2, 'Transporte'),
     (3, 'Lazer'),
     (4, 'Saúde'),
-    (5, 'Educação')
-ON CONFLICT (id) DO NOTHING;
+    (5, 'Educação');
 
 -- SQL Conta Financeira
 INSERT INTO conta_financeira (id, nome, saldo, tipo, usuario_id)
@@ -26,10 +24,4 @@ VALUES
     (4, 'Itaú', 150, 'DEBITO', 4),
     (5, 'Nubank', 3000, 'DEBITO', 3),
     (6, 'BB', 10, 'CREDITO', 2),
-    (7, 'PicPay', 1, 'CREDITO', 1)
-ON CONFLICT (id) DO NOTHING;
-
--- Resetar sequências para evitar conflito de chave primária
-SELECT setval('usuario_id_seq', (SELECT MAX(id) FROM usuario) + 1, false);
-SELECT setval('tipo_categoria_id_seq', (SELECT MAX(id) FROM tipo_categoria) + 1, false);
-SELECT setval('conta_financeira_id_seq', (SELECT MAX(id) FROM conta_financeira) + 1, false);
+    (7, 'PicPay', 1, 'CREDITO', 1);
