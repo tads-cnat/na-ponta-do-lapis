10d0fd744f6db61fe49c9538feac3683655c0615
Pedro <rpedroricardo87@gmail.com>
2026-04-24 16:50:10 -0300

    feat: add convite repository

---
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/ConviteRepository.java
@@ -0,0 +1,14 @@
+package com.npl.na_ponta_do_lapis.repository;
+
+import com.npl.na_ponta_do_lapis.entity.Convite;
+import com.npl.na_ponta_do_lapis.entity.Usuario;
+import com.npl.na_ponta_do_lapis.entity.enums.StatusConvite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ConviteRepository extends JpaRepository<Convite,Long> {
+    List<Convite> findByDestinatarioAndStatus(Usuario user, StatusConvite status);
+
+    boolean existsByDestinatarioAndStatus(Usuario user, StatusConvite status);
+}
