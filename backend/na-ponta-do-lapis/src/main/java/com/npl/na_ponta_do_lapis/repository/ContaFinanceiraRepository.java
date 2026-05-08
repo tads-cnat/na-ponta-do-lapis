fcc3278a98dd0d27fd3723a0b856e6d47188107f
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-08 00:16:00 -0300

    fix: refatorando tipoCategoria, tirando relacionamento dela com usuario

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/ContaFinanceiraRepository.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/ContaFinanceiraRepository.java
@@ -3,6 +3,12 @@ package com.npl.na_ponta_do_lapis.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.npl.na_ponta_do_lapis.entity.ContaFinanceira;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ContaFinanceiraRepository extends JpaRepository<ContaFinanceira, Long> {
+
+    @Query("SELECT c FROM ContaFinanceira c WHERE c.usuario.email = :email")
+    List<ContaFinanceira> buscarContaFinanceiraUsuarioLogado(String email);
 }
