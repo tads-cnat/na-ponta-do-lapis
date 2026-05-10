f3a80f4b18eca939066e9387f1873dff5d3c3f72
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-10 16:06:42 -0300

    feat: add header com logout e foto de user

--- frontend/na-ponta-do-lapis/src/app/model/IUsuario.models.ts
+++ frontend/na-ponta-do-lapis/src/app/model/IUsuario.models.ts
@@ -2,3 +2,12 @@ export interface UsuarioResume {
     id:number,
     nome:string
 }
+
+export interface Usuario {
+    id:number,
+    nome:string,
+    email:string,
+    username:string,
+    fotoPerfil:string,
+    papel:string
+} 
