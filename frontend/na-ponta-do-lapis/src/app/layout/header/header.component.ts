7c226d71676fc98d3bb1d07c2523722de90d961e
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-05-10 04:10:46 -0300

    feat: add toast

--- frontend/na-ponta-do-lapis/src/app/layout/header/header.component.ts
+++ frontend/na-ponta-do-lapis/src/app/layout/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { PrimeNGModuleModule } from '../../shared/primeNg.module';
+import { Route, Router } from '@angular/router';
+import { AuthService } from '../../auth/service/auth.service';
+import { StorageService } from '../../auth/service/storage.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimeNGModuleModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(private router:Router){}
+
+  logout(){
+    StorageService.logout()
+    this.router.navigateByUrl("/login")
+  }
+}
