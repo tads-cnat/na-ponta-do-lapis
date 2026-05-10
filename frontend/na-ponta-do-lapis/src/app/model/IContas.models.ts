1794c8fc42da185393aabc5ba5c66c6a86a37bc9
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-05-10 15:16:19 -0300

    fix: corrigindo modelo de interface de conta

--- frontend/na-ponta-do-lapis/src/app/model/IContas.models.ts
+++ frontend/na-ponta-do-lapis/src/app/model/IContas.models.ts
@@ -1,19 +1,16 @@
-export interface IContaFinanceira {
+export interface IContas {
     id: number | null,
     nome: string,
     saldo: number,
-    cor: string | undefined,
-    usuarioResume: UsuarioResume | null,
+    cor: string,
+    usuario: {
+      id: number;
+      nome: string;
+    };
 }
 
-export interface UsuarioResume {
-  id: number;
-  nome: string;
-}
-
-export interface IContaFinanceiraRequest {
+export interface IContasRequest {
     nome: string;
     saldo: number;
     cor: string;
-    idUsuario: number
 }
