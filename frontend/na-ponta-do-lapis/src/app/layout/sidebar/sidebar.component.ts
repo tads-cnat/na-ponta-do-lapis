19acf06a8e68f898e79cd8439cf63dbcddd8b8a5
Bruno Dias <brunoipb1@gmail.com>
2026-05-27 01:26:07 +0000

    fix: posiona side bar e remove bg

--- frontend/na-ponta-do-lapis/src/app/layout/sidebar/sidebar.component.ts
+++ frontend/na-ponta-do-lapis/src/app/layout/sidebar/sidebar.component.ts
@@ -11,7 +11,7 @@ import { IconComponent } from '../../shared/components/icon/icon.component';
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent implements OnInit {
-  aberto: boolean = true;
+  aberto: boolean = false;
   items: MenuItem[] | undefined ;
 
   ngOnInit(): void {
---
+++ frontend/na-ponta-do-lapis/src/app/pages/metas/metas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metas',
+  imports: [],
+  templateUrl: './metas.component.html',
+  styleUrl: './metas.component.css',
+})
+export class MetasComponent {
+  
+}
