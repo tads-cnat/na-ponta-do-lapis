dab27ac49b520c69c9093272665ae8c74131dfd9
ggVini <viniciussou222@gmail.com>
2026-06-01 00:24:11 -0300

    feat: criacao de pagina de perfil

---
+++ frontend/na-ponta-do-lapis/src/app/pages/perfil/perfil.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  imports: [],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.css',
+})
+export class PerfilComponent {}
