8c629776d4f3c02cc69eefe8ec907c101d2c2a8e
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-06-04 15:26:27 -0300

    feat: add requisição para mostrar saldo atual da conta

--- frontend/na-ponta-do-lapis/src/app/shared/components/card-saldo/card-saldo.component.ts
+++ frontend/na-ponta-do-lapis/src/app/shared/components/card-saldo/card-saldo.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PrimeNGModuleModule } from '../../primeNg.module';
+import { ContaFinanceiraService } from '../../../pages/contas/service/contas.service';
+import { IContas } from '../../../model/IContas.models';
+import { MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-saldo',
-  imports: [PrimeNGModuleModule],
+  imports: [PrimeNGModuleModule, CommonModule],
   templateUrl: './card-saldo.component.html',
   styleUrl: './card-saldo.component.css',
 })
-export class CardSaldoComponent {}
+export class CardSaldoComponent {
+
+  contaService = inject(ContaFinanceiraService);
+  private messageService = inject(MessageService)
+
+  contas:IContas[] = [];
+  saldoTotal:number = 0;
+
+  
+  ngOnInit(): void{
+    this.listarContas();
+  }
+
+  public listarContas(): void {
+    this.contaService.listarContasUsuarioLogado().subscribe({
+      next: (res:IContas[]) => {
+        this.contas = res;
+        console.log(this.contas)
+        this.saldoTotal = 0;
+        res.forEach(atual =>  this.saldoTotal += atual.saldo);
+      },
+      error: (err:Error) => {
+         console.log(err)
+        this.messageService.add({
+          severity: 'warn',
+          summary: 'Erro as carregar Dados da conta',
+          detail: '',
+          life: 2000
+        });
+      }
+    })
+  }
+
+}
