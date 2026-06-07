639f8b4dcc7c85725f179b8006173ff9d00d62ac
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-06-07 14:26:31 -0300

    Revert "feat: impl do filtro por descricao"
    This reverts commit 9501dc4ef4b80cdd9ef95a2d888ccc5756aab089.

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/TransacaoRepository.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/TransacaoRepository.java
@@ -3,7 +3,6 @@ package com.npl.na_ponta_do_lapis.repository;
 import com.npl.na_ponta_do_lapis.entity.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,5 +14,5 @@ public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
     @Query("SELECT  t FROM Transacao t WHERE t.contaFinanceira.usuario.email = :email " +
             "AND LOWER(t.descricao) LIKE LOWER(CONCAT('%', :descricao, '%')) " +
              "ORDER BY t.dataHora DESC")
-    List<Transacao> buscarPorDescricao(@Param("email") String email, @Param("descricao") String descricao);
+    List<Transacao> buscarPorDescricao(String email,String descricao);
 }
