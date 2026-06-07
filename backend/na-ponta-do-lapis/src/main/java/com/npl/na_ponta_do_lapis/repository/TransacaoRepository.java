67456294cab134ea566dede8369ac58d9f6d54d1
Lucaasshq <lucashenriquebarros2016@gmail.com>
2026-06-07 14:30:54 -0300

    impl: filtro de busca transacao por descricao

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/TransacaoRepository.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/repository/TransacaoRepository.java
@@ -3,6 +3,7 @@ package com.npl.na_ponta_do_lapis.repository;
 import com.npl.na_ponta_do_lapis.entity.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,5 +15,5 @@ public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
     @Query("SELECT  t FROM Transacao t WHERE t.contaFinanceira.usuario.email = :email " +
             "AND LOWER(t.descricao) LIKE LOWER(CONCAT('%', :descricao, '%')) " +
              "ORDER BY t.dataHora DESC")
-    List<Transacao> buscarPorDescricao(String email,String descricao);
+    List<Transacao> buscarPorDescricao(@Param("email") String email, @Param("descricao") String descricao);
 }
