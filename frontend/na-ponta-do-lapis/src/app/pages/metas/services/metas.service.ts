cf3760ce5c4c983dbf4093a285c4f98c7b5046de
Bruno Dias <brunoipb1@gmail.com>
2026-06-07 08:42:12 -0300

    feat: meta component

---
+++ frontend/na-ponta-do-lapis/src/app/pages/metas/services/metas.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MetasService {
+  private BASE_URL:string = "http://localhost:8080"
+
+  constructor(private http:HttpClient){}
+
+  public listarMetas(): Observable<any> {
+    return this.http.get<any>(`${this.BASE_URL}/metas`)
+  }
+
+
+
+}
