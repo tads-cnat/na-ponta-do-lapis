79b5a39bb75cbfa09494186fc78b544677afe00e
Pedro <rpedroricardo87@gmail.com>
2026-06-08 20:20:01 -0300

    feat: starting familia create page

--- frontend/na-ponta-do-lapis/src/app/pages/familia/familia.component.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/familia/familia.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Button } from "primeng/button";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-familia',
-  imports: [],
+  imports: [Button, FormsModule],
   templateUrl: './familia.component.html',
   styleUrl: './familia.component.css',
 })
-export class FamiliaComponent {}
+export class FamiliaComponent {
+  nomeFamilia: string = "";
+  submitted = false;
+
+  criarFamilia() {
+    this.submitted = true;
+    if (!this.nomeFamilia.trim()) return;
+    
+    // lógica de criar família...
+  }
+  resetNome(){
+      this.nomeFamilia = "";
+  }
+}
