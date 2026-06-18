d00692701916a755a12f8143e75a5ac9773e5667
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-06-18 20:01:19 -0300

    refactor: terminando de separar os componentes e adicionando inputs e outputs nas classes filhos

--- frontend/na-ponta-do-lapis/src/app/pages/contas/components/cartoes/acoes.component.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/contas/components/cartoes/acoes.component.ts
@@ -1,38 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from '@angular/core';
+import { PrimeNGModuleModule } from '../../../../shared/primeNg.module';
 
 @Component({
   selector: 'app-acoes-cartoes',
-  imports: [],
+  standalone: true,
+  imports: [PrimeNGModuleModule],
   template: `
     <div class="flex flex-wrap gap-3">
 
       <p-button
-          label="Adicionar"
-          icon="pi pi-plus"
-          severity="success"
-          class="rounded-xl"
-          (click)="abrirDialog()">
+        label="Adicionar"
+        icon="pi pi-plus"
+        severity="success"
+        class="rounded-xl"
+        (click)="onAdicionar.emit()">
       </p-button>
 
       <p-button
-          label="Editar"
-          icon="pi pi-pencil"
-          severity="contrast"
-          class="rounded-xl"
-          (click)="editarConta(contaSelecionada)">
+        label="Editar"
+        icon="pi pi-pencil"
+        severity="contrast"
+        class="rounded-xl"
+        (click)="onEditar.emit()">
       </p-button>
 
       <p-button
-          label="Excluir"
-          icon="pi pi-trash"
-          severity="danger"
-          class="rounded-xl"
-          (click)="abrirDialogExcluir(contaSelecionada)">
+        label="Excluir"
+        icon="pi pi-trash"
+        severity="danger"
+        class="rounded-xl"
+        (click)="onExcluir.emit()">
       </p-button>
 
-  </div>
+    </div>
   `
-
 })
+export class AcoesCartoesComponent {
 
-export class AcoesCartoesConta {}
+  @Output() onAdicionar = new EventEmitter<void>();
+
+  @Output() onEditar = new EventEmitter<void>();
+
+  @Output() onExcluir = new EventEmitter<void>();
+
+}
