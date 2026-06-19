77a715bbd2893ababd6f887e63e8786c67b529c1
Erick Carlos <erickcarlos003@gmail.com>
2026-06-19 14:15:44 +0000

    Criar rota e implementar navbar

--- frontend/na-ponta-do-lapis/src/app/app.routes.ts
+++ frontend/na-ponta-do-lapis/src/app/app.routes.ts
@@ -1,27 +1,33 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
 import { TransacoesComponent } from './pages/transacoes/transacoes.component';
 import { ContasComponent } from './pages/contas/contas.component';
 import { MetasComponent } from './pages/metas/metas.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { authGuard } from './core/guard/auth.guard';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
-import { SignupComponent } from './auth/components/signup/signup.component';
-
 
 export const routes: Routes = [
-    {path: "", redirectTo: "login", pathMatch:"full"},
-    {path: "login", component: LoginComponent},
-    {path: 'signup', component:SignupComponent},
+    { 
+        path: '', 
+        component: LandingPageComponent, 
+        pathMatch: 'full' 
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    
+    // Área interna protegida
     {
-        path: "app",
+        path: 'app',
         component: MainLayoutComponent,
         canActivate: [authGuard],
         children: [
-            {path: "transacoes", component: TransacoesComponent },
-            {path: "contas", component: ContasComponent },
-            {path: "metas", component: MetasComponent },
-
+            { path: 'transacoes', component: TransacoesComponent },
+            { path: 'contas', component: ContasComponent },
+            { path: 'metas', component: MetasComponent },
+            { path: '', redirectTo: 'transacoes', pathMatch: 'full' }
         ]
     },
-
+    { path: '**', redirectTo: '' }
 ];
---
+++ frontend/na-ponta-do-lapis/src/app/pages/landing-page/components/features/features.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features',
+  imports: [],
+  templateUrl: './features.component.html',
+  styleUrl: './features.component.css',
+})
+export class FeaturesComponent {}
