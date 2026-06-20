5773d12515e6bc4426c2535eb5085f6838e0f6d9
Erick <erickcarlos003@gmail.com>
2026-06-20 15:02:56 -0300

    feat(entity):adicionar rotas e imagens no navbar e herosection

--- frontend/na-ponta-do-lapis/src/app/pages/landing-page/components/hero-section/hero-section.component.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/landing-page/components/hero-section/hero-section.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
+
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.css',
+  styleUrls: ['./hero-section.component.css'] 
 })
-export class HeroSectionComponent {}
+
+export class HeroSectionComponent { }
