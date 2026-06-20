3014951ff6bb19f81b41b58825990fd63e165986
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-06-20 17:55:47 -0300

    feat: adição de exceção de conflito com o banco de dados

--- backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/exception/ApiExceptionHandler.java
+++ backend/na-ponta-do-lapis/src/main/java/com/npl/na_ponta_do_lapis/web/exception/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.npl.na_ponta_do_lapis.web.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,14 +14,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ApiExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(
-            MethodArgumentNotValidException ex) {
-
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> erros = new HashMap<>();
 
         ex.getBindingResult().getFieldErrors().forEach(error -> erros.put(error.getField(), error.getDefaultMessage()));
 
         return ResponseEntity.badRequest().body(erros);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrity(DataIntegrityViolationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body("Erro de integridade dos dados.");
     }
 
     @ExceptionHandler(NotFoundException.class)
