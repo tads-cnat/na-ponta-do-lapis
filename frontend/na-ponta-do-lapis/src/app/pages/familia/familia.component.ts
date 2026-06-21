56222ce0793e68439fe1405d9c9d29074908e5b2
Pedro <rpedroricardo87@gmail.com>
2026-06-20 21:33:18 -0300

    feat: starting invite member to familia logic

--- frontend/na-ponta-do-lapis/src/app/pages/familia/familia.component.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/familia/familia.component.ts
@@ -1,17 +1,56 @@
 import { Component } from '@angular/core';
 import { Button } from "primeng/button";
 import { FormsModule } from "@angular/forms";
+import { InputText } from "primeng/inputtext";
+import { IconComponent } from '../../shared/components/icon/icon.component';
+import { FamiliaService } from './service/familia.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-familia',
-  imports: [Button, FormsModule],
+  imports: [Button, FormsModule, InputText, IconComponent],
   templateUrl: './familia.component.html',
   styleUrl: './familia.component.css',
 })
 export class FamiliaComponent {
+  familiaService = inject(FamiliaService);
   nomeFamilia: string = "";
-  submitted = false;
-
+  convidados: string[] = [];
+  submitted: boolean = false;
+  value: string = "";
+  message: string = "";
+  limparConvidados() {
+    this.convidados = [];
+    console.log("Lista de convidados limpa");
+  }
+  adicionarNaListaConvidados(nickname:string) {
+    if (nickname.trim() === "") {
+      this.message = "O nickname não pode ser vazio.";
+      console.log("Nickname vazio");
+      return;
+    }
+    if (this.convidados.includes(nickname)) {
+      this.message = "Este usuário já foi adicionado à lista de convidados.";
+      console.log("Usuário já adicionado");
+      return;
+    }
+    if (nickname.trim() !== "") {
+      this.familiaService.verificarUsuarioExistente(nickname).subscribe(existe => {
+      if (existe) {
+          this.message = "Usuário não encontrado.";
+      } else {
+          this.convidados.push(nickname);
+          this.message = "";
+        }
+      });
+    this.value = "";
+    console.log("Usuário adicionado:", nickname);
+    }
+  }
+  removerConvidado(nickname:string) {
+    this.convidados = this.convidados.filter(convidado => convidado !== nickname);
+    console.log("Usuário removido:", nickname);
+  }
   criarFamilia() {
     this.submitted = true;
     if (!this.nomeFamilia.trim()) return;
--- frontend/na-ponta-do-lapis/src/app/pages/familia/service/familia.service.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/familia/service/familia.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/internal/operators/map';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class FamiliaService {}
+export class FamiliaService {
+  private BASE_URL:string = "http://localhost:8080"
+
+  constructor(private http:HttpClient){}
+
+  verificarUsuarioExistente(nickname: string): Observable<boolean> {
+     return this.http.get<any[]>(`${this.BASE_URL}/usuario/{nickname}`).pipe(
+      map(res => res.length === 0)
+     );
+  }
+
+}
