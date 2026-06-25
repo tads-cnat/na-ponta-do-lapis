d8003d11aaf7798d142bed984f911776faea41ca
Bruno Dias <dias.b@academico.ifrn.edu.br>
2026-06-25 00:26:34 +0000

    feat: add meta services

--- frontend/na-ponta-do-lapis/src/app/pages/metas/services/metas.service.ts
+++ frontend/na-ponta-do-lapis/src/app/pages/metas/services/metas.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '@env';
+import { Meta } from '@models/IMetas.models'
 
 
 @Injectable({
@@ -12,10 +13,24 @@ export class MetasService {
 
   constructor(private http:HttpClient){}
 
-  public listarMetas(): Observable<any> {
-    return this.http.get<any>(`${this.BASE_URL}/metas`)
+  public listarMetas(): Observable<Meta> {
+    return this.http.get<Meta>(`${this.BASE_URL}/metas`)
   }
 
+  public buscarMeta(id: number): Observable<Meta> {
+    return this.http.get<Meta>(`${this.BASE_URL}/metas/${id}`)
+  }
+  
+  public criarMeta(meta: Meta): Observable<Meta> {
+    return this.http.post<Meta>(`${this.BASE_URL}/metas`, meta)
+  }
 
+  public atualizarMeta(id: number, meta: Meta): Observable<Meta> {
+    return this.http.put<Meta>(`${this.BASE_URL}/metas/${id}`, meta)
+  }
+  
+  public deletarMeta(id: number): Observable<Meta> {
+    return this.http.delete<Meta>(`${this.BASE_URL}/metas/${id}`)
+  }
 
 }
